6b9a97c74eed7aecb83b9a068e76877afe16e039
saniru <santiago.nicolas.ruberto@gmail.com>
2023-01-03 18:11:18 +0100

    Install YouTube API and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import TrackManager from './TrackManager';
 function App() {
+  useEffect(()=>{const tag  = document.createElement("script");
+      tag.src = "https://www.youtube.com/iframe_api";
+      const firstScriptTag = document.getElementsByTagName("script")[0];
+      if (firstScriptTag.parentNode) {
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);  
+      }});
   return (
     <div className="App">
       <TrackManager/>
