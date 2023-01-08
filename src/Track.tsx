13c88d976cf4c1d929387834f985c353596f6509
saniru <santiago.nicolas.ruberto@gmail.com>
2023-01-08 20:00:12 +0100

    Add At-will mode to tracks

--- src/Track.tsx
+++ src/Track.tsx
@@ -8,9 +8,14 @@ interface TrackProps{
   name:string;
   TrackType:string;
 }
+enum TrackMode{
+  Random,
+  AtWill,
+}
 interface TrackState{
   volume:number;
   delay:number;
+  mode:TrackMode;
 }
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_number_between_two_values
 function getRandomArbitrary(min : number, max : number) {
@@ -27,6 +32,7 @@ export class Track extends Component {
     this.state = {
       volume:50,
       delay:this.props.delay,
+      mode:TrackMode.Random
     };
   }
   componentDidMount = () => {
@@ -37,7 +43,7 @@ export class Track extends Component {
     clearTimeout(this.TrackTimer.current);
   };
   randomPlay = () => {
-    if (this.props.playing) {
+    if (this.props.playing && this.state.mode == TrackMode.Random) {
       this.props.playTrack(this.props.id);
     }
     const rand = getRandomArbitrary(this.props.delay/2, this.props.delay);
@@ -52,14 +58,21 @@ export class Track extends Component {
     this.props.dispatch({type:"delay",payload:{id:this.props.id,val:this.state.delay}});
   };
   render() {
+    const buttons = {[TrackMode.Random]:<button onClick={() => this.props.dispatch({type:"playing",payload:{id:this.props.id}})}
+                                                name="playing">{this.props.playing ? "Pause" : "Play"}</button>,
+                     [TrackMode.AtWill]:<button onClick={() => this.props.playTrack(this.props.id)}>Play</button>};
+    const titles = { [TrackMode.Random]: <h3>Randomly playing</h3> ,
+                     [TrackMode.AtWill]:<h3>Playing at will</h3>};
     return (
       <div>
         <pre>{this.props.name}</pre>
         <pre>{this.props.TrackType}</pre>
+        <pre>{titles[this.state.mode]}</pre>
+        <button onClick={() => this.setState((state:TrackState, _) => ({
+                  mode: (state.mode + 1) % (Object.keys(TrackMode).length / 2)
+        }))}>Switch Mode</button>
         <input onChange={(e) => this.handleVolume(e)} name="volume" type="range" min="1" max="100"/>
         <input onChange={(e) => this.handleDelay(e)} name="delay" type="range" min={30*1000} max={30*60*1000} step={1000} />
-        <button onClick={() => this.props.dispatch({type:"playing",payload:{id:this.props.id}})}
-                name="playing">{this.props.playing ? "Pause" : "Play"}</button>
         <button onClick={() => this.props.dispatch({type:"remove",payload:{id:this.props.id}})}>Remove</button>
       </div>
     );
