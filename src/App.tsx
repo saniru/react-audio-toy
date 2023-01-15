325df760845397a5569a4a68bec9b4a0377c80c6
saniru <santiago.nicolas.ruberto@gmail.com>
2023-01-15 15:42:50 +0100

    Better checking for the script tag in the YT API load

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { useEffect } from 'react';
 import './App.css';
 import TrackManager from './TrackManager';
 function App() {
-  useEffect(()=>{const tag  = document.createElement("script");
-      tag.src = "https://www.youtube.com/iframe_api";
-      const firstScriptTag = document.getElementsByTagName("script")[0];
-      if (firstScriptTag.parentNode) {
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);  
-      }});
+  useEffect(()=>{
+    const tag  = document.createElement("script");
+    tag.src = "https://www.youtube.com/iframe_api";
+    const firstScriptTag = document.getElementsByTagName("script")[0];
+    if (firstScriptTag && firstScriptTag.parentNode) {
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    }});
   return (
     <div className="App">
       <TrackManager/>
